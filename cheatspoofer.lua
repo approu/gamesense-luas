c31db715d825d65341e454a380cd3afc9d695344
approu <140549225+approu@users.noreply.github.com>
2026-09-18 18:31:19 +0300

    cheat spoofer means fool cheat revealers etc

---
+++ cheatspoofer.lua
@@ -0,0 +1,36 @@
+local ffi = require "ffi"
+
+ffi.cdef[[
+    typedef struct {
+        char pad0[8];
+        int32_t client;
+        int32_t audible_mask;
+        uint32_t xuid_low;
+        uint32_t xuid_high;
+        void* voice_data;
+        bool proximity;
+        bool caster;
+        char pad1[2];
+        int32_t format;
+        int32_t sequence_bytes;
+        uint32_t section_number;
+        uint32_t uncompressed_sample_offset;
+    } voice_data_t;
+]]
+
+client.set_event_callback("voice", function(e)
+    local p = ffi.cast("voice_data_t*", e.data)
+    if not p then return end
+
+    local target = p.client + 1
+    if target ~= entity.get_local_player() then
+        return -- чужие пакеты не трогаем
+    end
+
+    -- только свои
+    p.xuid_high = 0
+    p.xuid_low = 0
+    p.sequence_bytes = 0
+    p.section_number = 0
+    p.uncompressed_sample_offset = 0
+end)
